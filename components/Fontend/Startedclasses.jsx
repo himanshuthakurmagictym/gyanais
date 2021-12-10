269c52d71d3b78da75ecd15846dc3a413043a096
Magictym <magictym@outlook.com>
2021-12-10 17:58:35 +0530

    ss

--- components/Fontend/Startedclasses.jsx
+++ components/Fontend/Startedclasses.jsx
@@ -9,7 +9,7 @@ function Startedclasses({recentclasses}) {
     <section className="features11 cid-qKSpeMafIm" id="features11-d">
    
 <div className="container">
-<h2 className="mbr-fonts-style mbr-section-title align-center  display-2">All Courses</h2>
+<h2 className="mbr-fonts-style mbr-section-title align-center  display-2">Recently Started Courses</h2>
 <h3 className="mbr-fonts-style mbr-section-subtitle align-center mbr-light pt-3 display-7">We also offer services in the live class, doubts, chat, paid and utilization of signage.</h3>
 <div className="row justify-content-center pt-4">
 {recentclasses.slice(0, 4).map((Soonclasses) => { 
