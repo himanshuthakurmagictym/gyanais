69797f8ccd9249371617dd63369d3d9967241ed1
Magictym <magictym@outlook.com>
2021-12-22 17:31:23 +0530

    starting student panel

---
+++ pages/student/setting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function setting() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default setting
---
+++ pages/teacher/signout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function signout() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default signout
