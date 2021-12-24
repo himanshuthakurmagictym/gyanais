84901001c14b85dd25abb962987135ac139e6fe1
Magictym <magictym@outlook.com>
2021-12-24 18:10:35 +0530

    sdfsfda

--- pages/student/profile.js
+++ pages/student/profile.js
@@ -1,27 +1,59 @@
 import React from 'react'
 import Brudcrums from "../../components/Fontend/Brudcrums"
 import { parseCookies } from 'nookies'
-
-function profile() {
+var CryptoJS = require("crypto-js");
+import APIs from '../../config.js';
+function Profile({datas}) {
 
    
 
     return (
         <div>
             <Brudcrums/>
-            hello student
+         { 
+               datas.email ? datas.email : ""
+        }
         </div>
     )
 }
 
-export default profile
+export default Profile
 
 export async function getServerSideProps(ctx){
-    
-   const {user} = parseCookies(ctx)
-   console.log("sdsd"+(user))
+    const cookie = parseCookies(ctx)
 
-   return {
-       props:{}
-   }
+    if(!cookie.token){
+        
+        return {
+            redirect: {
+              permanent: false,
+              destination: "/login",
+            },
+            props:{},
+          };
+
+    }else{
+
+        var bytesss  = CryptoJS.AES.decrypt(cookie.token, '619619');
+        var token = JSON.parse(bytesss.toString(CryptoJS.enc.Utf8));
+
+        const sendData = JSON.stringify({token:token})
+        const res = await fetch(`${APIs.base_url}auth/verifytoken`, {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:sendData,
+       });
+       const datas =  await res.json();
+
+       return {
+        props: {datas: datas.data}
+     }
+
+    }
+    
+
+  
 }
+
--- pages/student/setting.js
+++ pages/student/setting.js
@@ -9,3 +9,40 @@ function setting() {
 }
 
 export default setting
+export async function getServerSideProps(ctx){
+    const cookie = parseCookies(ctx)
+
+    if(!cookie.token){
+        
+        return {
+            redirect: {
+              permanent: false,
+              destination: "/login",
+            },
+            props:{},
+          };
+
+    }else{
+
+        var bytesss  = CryptoJS.AES.decrypt(cookie.token, '619619');
+        var token = JSON.parse(bytesss.toString(CryptoJS.enc.Utf8));
+
+        const sendData = JSON.stringify({token:token})
+        const res = await fetch(`${APIs.base_url}auth/verifytoken`, {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:sendData,
+       });
+       const datas =  await res.json();
+
+       return {
+        props: {datas: datas.data}
+     }
+
+    }
+    
+
+  
+}
