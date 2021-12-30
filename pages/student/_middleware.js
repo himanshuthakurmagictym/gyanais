8e682e8e8e8a474fb06c39ecf47ec48ca318b4ec
Magictym <magictym@outlook.com>
2021-12-30 17:51:53 +0530

    profile page

--- pages/student/_middleware.js
+++ pages/student/_middleware.js
@@ -2,14 +2,12 @@
 import  { NextResponse  } from 'next/server'
 import { parseCookies } from 'nookies'
 import APIs from '../../config.js';
-
+import CryptoJS from "crypto-js"
 export async function middleware(req) {
 
   const tokend = JSON.stringify(req.cookies.token);
-
+  
   if(tokend){
-    
-    //const sendData = token:tokend;
     const sendData = JSON.stringify({token:req.cookies.token})
         const res = await fetch(`${APIs.base_url}auth/verifytoken`, {
             method:"POST",
@@ -18,7 +16,7 @@ export async function middleware(req) {
             },
             body:sendData,
        }).then(res => res.json())
-       .then(res => console.log(res))
+       .then(res => res)
   
        
   return NextResponse.next()
--- pages/student/profile.js
+++ pages/student/profile.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Brudcrums from "../../components/Fontend/Brudcrums"
 import { parseCookies } from 'nookies'
-var CryptoJS = require("crypto-js");
 import APIs from '../../config.js';
+import Image from 'next/image'
 
 function Profile({datas}) {
 
@@ -11,9 +11,51 @@ function Profile({datas}) {
     return (
         <div>
             <Brudcrums/>
-         { 
-            //    datas.email ? datas.email : ""
-        }
+            <section>
+              <div className="container">
+              <section className="testimonials2 cid-qKSrnk6eVJ" id="testimonials2-e">
+<div className="container">   
+    <div className="row justify-content-center pt-2"> 
+        <div className="card col-12 col-md-12">
+        <div className="card-box">
+        <div className="row"> 
+        
+                        <div className="col-md-4">
+                       
+                                 <Image src={`/assets/images/`}  width='200' height='200' alt="currentaffair"/>
+                        </div>
+                    
+
+                        <div className=" col-md-8">
+                        
+                                <h3 className="mbr-fonts-style mb-3 align-left display-2">{datas.firstname} {datas.lastname}</h3>
+                                <p className="mbr-text card-text mbr-fonts-style align-left m-0 display-7">
+                                {datas.username}
+                                </p>
+
+                                <p className="mbr-text card-text mbr-fonts-style align-left m-0 display-7">
+                                {datas.email} 39K Watch mins
+                                </p>
+                                <div className="underline align-left">
+                                <div className="line"></div>
+                        
+                       
+                        </div>
+                        </div>
+                </div>
+            </div>
+           
+        </div>
+
+
+
+    </div>
+</div>
+</section>
+             
+           
+              </div>
+           </section>
         </div>
     )
 }
@@ -21,38 +63,36 @@ function Profile({datas}) {
 export default Profile
 
 export async function getServerSideProps(ctx){
-    const cookie = await parseCookies(ctx)
-    return{ props:{}}
-    // if(!cookie.token){
+    const cookie = await parseCookies(ctx);
+  
+    if(!cookie.token){
         
-    //     return {
-    //         redirect: {
-    //           permanent: false,
-    //           destination: "/login",
-    //         },
-    //         props:{},
-    //       };
+        return {
+            redirect: {
+              permanent: false,
+              destination: "/login",
+            },
+            props:{},
+          };
 
-    // }else{
+    }else{
        
-        // var bytesss  = CryptoJS.AES.decrypt(cookie.token, '619619');
-        // var token = JSON.parse(bytesss.toString(CryptoJS.enc.Utf8));
-        //console.log(token)
-    //     const sendData = JSON.stringify({token:cookie.token})
-    //     const res = await fetch(`${APIs.base_url}auth/verifytoken`, {
-    //         method:"POST",
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },
-    //         body:sendData,
-    //    });
-    //    const datas =  await res.json();
-    //     console.log("sd"+datas);
-    //    return {
-    //     props: {datas: datas.data}
-    //  }
+      
+        const sendData = JSON.stringify({token:cookie.token})
+        const res = await fetch(`${APIs.base_url}auth/verifytoken`, {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:sendData,
+       });
+       const datas =  await res.json();
+      
+       return {
+        props: {datas: datas.data}
+     }
 
-    // }
+    }
     
 
   
