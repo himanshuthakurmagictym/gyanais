d648a30aa5179c426ad14a4c181028c831807dba
himanshuthakurmagictym <himanshu@magictym.com>
2022-01-11 17:10:43 +0530

    subscriptionpage

--- pages/student/subscription/[categoryid].js
+++ pages/student/subscription/[categoryid].js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import Brudcrums from "../../../components/Fontend/Brudcrums"
 import APIs from '../../../config.js';
 import {useRouter} from 'next/router'
-function Subscription() {
+function Subscription({packages, categoryName}) {
     const router = useRouter();
     const {categoryid} = router.query;
     return (
@@ -12,34 +12,28 @@ function Subscription() {
              <Brudcrums/>
              <section className="testimonials2 cid-qKSrnk6eVJ" id="testimonials2-e">
              <div className='container'>
-             <h2 className="mbr-fonts-style mb-3 align-center display-2">Student Payment Plan for {categoryid}</h2>
-                 <div className='row'>
-                 <div className='bodycontent col-md-4'>
-                        <h4 className="card-title mbr-fonts-style align-center mb-0 mbr-white display-5">
-                                Plan 1
+             <h2 className="mbr-fonts-style mbr-section-title align-center  display-2 mb-5">Payment Plan for {categoryName}</h2>
+                 <div className='row '>
+               {packages.map((subscription)=>(
+                        <div className='bodycontent col-md-4'>
+                        <h4 className="card-titles mbr-fonts-style align-center mb-0 mbr-white display-5">
+                            {subscription.packageName}   
                         </h4>
 
-                        <p className="mbr-text card-text mbr-fonts-style align-center m-0 display-7" >
-                               <p> Basic plan to get you started</p>
-                            <ul className=''>
-                                <li>India's best educators</li>
-                                <li>Interactive live classes</li>
-                                <li>Live tests & quizzes</li>
-                                <li>Structured courses & PDFs</li>
-                            </ul>
-                           <button className='btn btn-md btn-info display-4 btn-success'>Get Subscription</button>
-                            </p>
-                  </div>
-                  <div className='bodycontent col-md-4'>
-                            <h4 className="card-title mbr-fonts-style align-center mb-0 mbr-white display-5">
-                                    Plan 2
-                            </h4>
-                  </div>
-                  <div className='bodycontent col-md-4'>
-                            <h4 className="card-title mbr-fonts-style align-center mb-0 mbr-white display-5">
-                                    Plan 3
-                            </h4>
-                  </div>
+                        <p className="mbr-text card-text mbr-fonts-style mbr-white align-center m-0 display-7" >
+                                <p> {subscription.packageDesc} </p>
+                                
+                             <ul className='align-left'>
+                             <li>India's best educators</li><li>Interactive live classes</li><li>Structured courses &amp; PDFs</li>
+
+                            {/* <ul class="align-left"><li>India's best educators</li><li>Interactive live classes</li><li>Live tests &amp; quizzes</li><li>Structured courses &amp; PDFs</li></ul> */}
+                            </ul> 
+                            <h4>Rs.{subscription.packageAmount}</h4>
+                            <a href={subscription._id}><button className='btn btn-md btn-info display-4 btn-success'>Get Subscription</button>
+                            </a></p>
+                        </div>
+               ))}
+                 
                  </div>
              </div>
              </section>
@@ -48,3 +42,17 @@ function Subscription() {
 }
 
 export default Subscription
+
+export const getServerSideProps = async (context) => {
+const {params} =  context
+const res = await fetch(`${APIs.base_url}subscription/${params.categoryid}`);
+const datas = await res.json()
+
+
+const category = await fetch(`${APIs.base_url}courseCategory/${params.categoryid}`);
+const result = await category.json();
+const categoryName = result.data.course_category_name;
+ return {
+     props:{packages:datas.data, categoryName:categoryName}
+ }
+}
