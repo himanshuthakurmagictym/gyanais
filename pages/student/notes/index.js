435658fa87cc134e91ca5a593f6b00cbd3a9c2c2
himanshuthakurmagictym <himanshu@magictym.com>
2022-01-15 19:33:35 +0530

    save

--- pages/student/notes/index.js
+++ pages/student/notes/index.js
@@ -11,54 +11,46 @@ import {fetchUser} from '../../../lib/user'
 import Sidebar from '../../../components/Fontend/sidebar';
 import Notes from '../../../components/Fontend/Classes/notes';
 
-function Category({allnotes, categoryid}) {
+function index({note}) {
+  
 
 
-    return (
-        <>
-        <Brudcrums />
-        
-        <section className="features11 cid-qKSpeMafIm  cid-qKSrnk6ess pt-5" id="features11-d">
-            <div className="container">
-            <h2 className="mbr-fonts-style mbr-section-title align-center  display-2">All Notes</h2>
-            <h3 className="mbr-fonts-style mbr-section-subtitle align-center mbr-light pt-3 display-7">We also offer services in the live class, doubts, chat, paid and utilization of signage.</h3>
-        
-            <div className="row justify-content-center pt-4">
-
-                     <div className="col-md-3">
-                     <Sidebar categoryid ={categoryid}/>
+        return (
+            <>
+            <Brudcrums />
+            
+            <section className="features11 cid-qKSpeMafIm  cid-qKSrnk6ess pt-5" id="features11-d">
+                <div className="container">
+                <h2 className="mbr-fonts-style mbr-section-title align-center  display-2">All Notes</h2>
+                <h3 className="mbr-fonts-style mbr-section-subtitle align-center mbr-light pt-3 display-7">We also offer services in the live class, doubts, chat, paid and utilization of signage.</h3>
+            
+                <div className="row justify-content-center pt-4">
+    
+                         <div className="col-md-3">
+                         <Sidebar categoryid ={note.categoryid}/>
+                            </div>
+    
+                            <div className="col-md-9">
+                            <div className="card-box">
+                               {note.description}
+                            </div>
                         </div>
-
-                        <div className="col-md-9">
-                        <div className="card-box">
-                            {/* {allnotes.map((notes) => (
-                               <Notes notes={notes}/>
-
-                            ))} */}
                         </div>
-                    </div>
-                    </div>
-           
-            </div>
-        </section>  
-        </>
-    )
+               
+                </div>
+            </section>  
+            </>
+        )
+   
 }
 
-export default Category
-
+export default index
 export const getServerSideProps = async (context) =>{
     const { params } = context;
     const {categoryid } = params;
-    const res = await fetch(`${APIs.base_url}notes/categoryid`,{
-        headers:{
-            "Content-Types":"application/json"
-        },
-        method:"POST",
-        body:JSON.stringify({category_id:categoryid})
-    });
+    const res = await fetch(`${APIs.base_url}student/notes/note/${categoryid}`);
     const datas = await res.json()
-    console.log(datas) 
+   
         const URLS = APIs.base_url+"payment/status";
         const sendData = JSON.stringify({  category_id: categoryid,  user: context.req.cookies['cid'] })
         const ress = await fetch(URLS, {
@@ -85,7 +77,7 @@ export const getServerSideProps = async (context) =>{
 
                 return {
                     props: {
-                        allnotes: datas.data || '',
+                        note: datas.data,
                         categoryid
                     }
                 }
@@ -96,7 +88,7 @@ export const getServerSideProps = async (context) =>{
 
 
     
-    
+    //console.log(datas) 
    
 
 }
