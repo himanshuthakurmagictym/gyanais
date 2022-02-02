0ff78c5fd7001e1692feca966af40db7f57dac5c
himanshuthakurmagictym <himanshu@magictym.com>
2022-02-02 17:05:34 +0530

    save

--- components/Fontend/Layout.jsx
+++ components/Fontend/Layout.jsx
@@ -36,7 +36,7 @@ export default function Layout({children}) {
 
  return(
     <AppContext.Provider value={ress}>
-         <Header socket={socket}/>
+         <Header />
          {children}
          <Footer /> 
          </AppContext.Provider>
