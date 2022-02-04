e966a558a0c63375ac6b2a8d2ffdca5fd003ec5b
himanshuthakurmagictym <himanshu@magictym.com>
2022-02-04 14:06:35 +0530

    save

--- components/Fontend/sidebar.jsx
+++ components/Fontend/sidebar.jsx
@@ -6,11 +6,11 @@ function sidebar({categoryid}) {
             <div className="left_navigation">
                                
                                <ul>
-                                   <Link href={`/student/category/${categoryid}`}><li>Course</li></Link>
-                                   <Link href={`/student/notes/${categoryid}`}><li>Notes</li></Link>
-                                   <Link href={`/student/freelive/${categoryid}`}><li>Free Live Classes</li></Link>
-                                   <Link href={`/student/syllabuses/${categoryid}`}><li>Syllabus</li></Link>
-                                   <Link href={`/student/mcqs/${categoryid}`}><li>MCQ Practice</li></Link>
+                                   <Link href={`/student/category/${categoryid}`} passHref><li className='cursorlink'>Course</li></Link>
+                                   <Link href={`/student/notes/${categoryid}`} passHref><li className='cursorlink'>Notes</li></Link>
+                                   <Link href={`/student/freelive/${categoryid}`} passHref><li className='cursorlink'>Free Live Classes</li></Link>
+                                   <Link href={`/student/syllabuses/${categoryid}`} passHref><li className='cursorlink'>Syllabus</li></Link>
+                                   <Link href={`/student/mcqs/${categoryid}`} passHref><li className='cursorlink'>MCQ Practice</li></Link>
                                    {/* <a href="/student/profile"><li>Subscription</li> </a>  */}
                                </ul>
 
