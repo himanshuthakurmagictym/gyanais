ba9cddeae8a861f4cdd9f38c6be9f89929e6efc1
Magictym <magictym@outlook.com>
2022-05-13 18:59:29 +0530

    sdsds

--- components/Fontend/Whiteboard.jsx
+++ components/Fontend/Whiteboard.jsx
@@ -281,7 +281,7 @@ body:JSON.stringify({userid:userid, question:pollmodel._id, answer:option, corre
     const canvas = canvasRef.current;
       const contexts = canvas.getContext('2d');
       // imageRef?.current?.onload = () =>{
-      imageRef?.current?.crossOrigin = "Anonymous";
+      // imageRef?.current?.crossOrigin = "Anonymous";
        contexts?.drawImage(imageRef?.current, 0, 0, canvas.width, canvas.height)
       // }
     },[socket][imageRef])
