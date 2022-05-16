964093c109e8214ec492a1c5abdc184d5aaa08e7
Magictym <magictym@outlook.com>
2022-05-16 15:28:27 +0530

    save

--- pages/currentaffairs.js
+++ pages/currentaffairs.js
@@ -19,18 +19,19 @@ function currentaffairs({allcurrentaffairs}) {
         {allcurrentaffairs.map((currentAffairs) =>(
                
         <div className="card col-12 col-md-10" key="{currentAffairs._id}">
-            <Link href={`/currentaffairsDetails/${currentAffairs.currentaffair_slug}`} passHref >
+            <Link href={`/currentaffairsDetails/${currentAffairs._id}`} passHref >
             <div className="card-box">
                 <div className="card-header">
                     <div className="card-img">                       
-                        <Image src={`/assets/images/currentaffairs/${currentAffairs.currentaffair_image}`} layout='fill' alt="currentaffairs" />
+                        <Image src={`${APIs.base_url_home}${currentAffairs.currentaffair_image}`} layout='fill' alt="currentaffairs" />
                     </div>
                     <h4 className="card-title mbr-fonts-style align-center mb-0 mbr-white display-5">
                    {currentAffairs.currentaffair_title}
                     </h4>
                 </div>
                 <p className="mbr-text card-text mbr-fonts-style align-left m-0 display-7">
-                {currentAffairs.currentaffair_summary} 
+                <p dangerouslySetInnerHTML={{ __html:currentAffairs.currentaffair_summary }} />
+    
                 </p>
                 <div className="underline align-left">
                      <div className="line"></div>
@@ -58,7 +59,7 @@ export default currentaffairs
 export async function getServerSideProps(context) {
     const res = await fetch(`${APIs.base_url}currentaffair/all`);
     const datas = await res.json();
-    console.log(datas.data)
+
     return {
         props: {
             allcurrentaffairs: datas.data,
