d374aeb6bde45f138b0fcc161d36da03b4786479
Magictym <magictym@outlook.com>
2022-05-17 12:09:56 +0530

    save

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
-//test url
+
+
+// if(process.env.NODE_ENV !== "production"){
+// //test url
 // const base_url = 'http://localhost:5000/api/';
 // const base_url_home = 'http://localhost:5000/';
-//live url
-const base_url = 'https://gyaanapi.herokuapp.com/api/';
-const base_url_home = 'https://gyaanapi.herokuapp.com/';
+// }else{
+// //live url
+// const base_url = 'https://gyaanapi.herokuapp.com/api/';
+// const base_url_home = 'https://gyaanapi.herokuapp.com/';
+// }
+const base_url = (process.env.NODE_ENV == "production")?'https://gyaanapi.herokuapp.com/api/':'http://localhost:5000/api/';
+const base_url_home = (process.env.NODE_ENV == "production")?'https://gyaanapi.herokuapp.com/':'http://localhost:5000/';
+
  const roles = ['Teacher', 'Student', 'Admin'];
  const RAZORPAY_KEY= 'rzp_test_ukrfVTcdYzOvVw';
 
