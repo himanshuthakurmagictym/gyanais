7526e59df19e6bf86ae436a1077c10fadf43f1cc
Magictym <magictym@outlook.com>
2022-06-03 12:10:16 +0530

    save

--- components/Fontend/Webcameras.jsx
+++ components/Fontend/Webcameras.jsx
@@ -324,7 +324,7 @@ const stoprecording = ()=>{
        :
        <>
         <div className="camerabutton">
-        <button onClick={e=>{enableAudio()}}>{(audiovalue == 1)? "Audio Disabled":" Audio Enabled"}</button>
+        <button className="btn-info" onClick={e=>{enableAudio()}}>{(audiovalue == 1)? "Audio Disabled":" Audio Enabled"}</button>
         </div>
         <video ref={clientvideoscream} autoPlay playsInline muted width='100%' height='100%'/>
        </>
